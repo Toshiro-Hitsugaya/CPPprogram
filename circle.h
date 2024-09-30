d2522a80c3c33d96a42159ba0c6b8f6b676eee73
asimpcoder <231880203@smail.nju.edu.cn>
2024-09-30 17:15:55 +0800

    Update circle.h

--- circle.h
+++ circle.h
@@ -1,5 +1,5 @@
 #include <def.h>
-#include <Monster.h>
+#include <monster.h>
 class Coor
 {
 public:
@@ -11,12 +11,16 @@ public:
     }
 };
 
-class circle
+class circle:public QObject
 {
+    Q_OBJECT
 private:
     Coor center;
     double duration;
     double R;
+    QPixmap CircleImage; // 圈图像
+    QGraphicsPixmapItem* graphicsItem; // 图像项
+
     bool fire;
     double fire_dps;
 
@@ -25,10 +29,10 @@ private:
     
 
 public:
-    circle(double x, double y, int type);
+    circle(double x, double y, int type, const QPixmap& image, QGraphicsScene* scene);
     bool isIn(double x, double y);
-
-    void fire_attack(Monster M);
+    void start_duration();
+    void fire_attack(Monster& M);
 
     Coor getTopLeftCorner() const {
         return Coor(center.x - R, center.y - R);
@@ -40,8 +44,9 @@ public:
     ~circle();
 };
 
-circle::circle(double x, double y, int type):center(x, y)
+circle::circle(double x, double y, int type, const QPixmap& image, QGraphicsScene* scene):center(x, y), CircleImage(image), graphicsItem(new QGraphicsPixmapItem(image))
 {
+    scene->addItem(graphicsItem);
     fire=type==FIRE;
     ice=type==ICE;
     switch (level)
@@ -73,12 +78,24 @@ inline bool circle::isIn(double x, double y)
         return distanceSqr < RSqr;
 }
 
-inline void circle::fire_attack(Monster M)
+inline void circle::start_duration()
 {
-    if(isIn(M.get_x(), M.get_y()))
-        M.attacked(fire_dps);
+    QTimer::singleShot(duration * 1000, this, [this]() {
+        this->~circle();
+    });
+}
+
+inline void circle::fire_attack(Monster& M)
+{
+    QTimer* attackTimer = new QTimer(this);
+    connect(attackTimer, &QTimer::timeout, this, [this, &M, attackTimer]() {
+            if (isIn(M.get_x(), M.get_y())) 
+                M.takeDamage(fire_dps); // 对怪物造成伤害
+        });
+    attackTimer->start(1000); // 每秒触发一次
 }
 
 circle::~circle()
 {
+    graphicsItem->hide();
 }
