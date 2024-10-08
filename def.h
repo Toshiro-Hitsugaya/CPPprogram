24e7c1adc6d8b2ade571c9d96dc36645451b8638
asimpcoder <231880203@smail.nju.edu.cn>
2024-10-08 22:30:04 +0800

    Update def.h

--- def.h
+++ def.h
@@ -1,25 +1,32 @@
-#define CELL_SIZE_0 60
-#define ROW_0 6
-#define COLUMN_0 10
-
-
+#ifndef DEF_H
+#define DEF_H
+class Coor
+{
+public:
+    double x;
+    double y;
+    Coor(double x, double y){
+        this->x=x;
+        this->y=y;
+    }
+};
 #define LEVEL_0 0
 #define LEVEL_1 1
 
-#define R_0 1.0
+#define R_0 75.0
 #define R_1 2.0
 
 
-#define DURATION_0 5.0
+#define DURATION_0 100.0
 #define DURATION_1 7.5
 
 
 #define FIRE 100
-#define FIRE_DPS_0 5.0
-#define FIRE_DPS_1 10.0
+#define FIRE_DPH_0 1.0
+#define FIRE_DPH_1 10.0
 
 #define ICE 101
 #define DECELERATION_0 5.0
 #define DECELERATION_1 8.0
-
-int level;
+static int level;
+#endif
